c6566a8cf18d6e101034ff0e05d8d42a9fdd3281
VITOR COSTA CARDOSO DA LUZ <vitor_c_luz@estudante.sesisenai.org.br>
2025-04-23 09:41:08 -0300

    terceiro commit

--- fs.module.js
+++ fs.module.js
@@ -1,11 +1,8 @@
 const fs = require ('fs')
 
-console.log('antes')
-try{
-    const data = fs.readFileSync('./teste.txt','utf-8')
-    console.log(data)
-} catch (e) {
-    console.log(e)
-}
-
-console.log('depois')
+const texto = 'abcdefghijklmnopqrstuvwxyz'
+fs.writeFile('./teste.txt', texto, (err) => {
+    if (err){
+    console.log(err)
+    }
+})
